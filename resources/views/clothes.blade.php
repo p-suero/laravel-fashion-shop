fd619ce51c7d241a583dd39fa2526f835f9279e6
Paolo <paolo.suero@gmail.com>
2020-07-07 09:51:33 +0200

    Esercizio completo

--- resources/views/clothes.blade.php
+++ resources/views/clothes.blade.php
@@ -6,36 +6,38 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Item</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Brand</th>
-                        <th scope="col">Collection Year</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($clothes as $clothe)
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td>
-                                {{$clothe->id}}
-                            </td>
-                            <td>
-                                {{$clothe->item}}
-                            </td>
-                            <td>
-                                {{$clothe->price}}
-                            </td>
-                            <td>
-                                {{$clothe->brand}}
-                            </td>
-                            <td>
-                                {{$clothe->collection_year}}
-                            </td>
+                            <th scope="col">ID</th>
+                            <th scope="col">Item</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Brand</th>
+                            <th scope="col">Collection Year</th>
                         </tr>
-                    @endforeach
-                </tbody>
+                    </thead>
+                    <tbody>
+                        @foreach ($clothes as $clothe)
+                            <tr>
+                                <td>
+                                    {{$clothe->id}}
+                                </td>
+                                <td>
+                                    {{$clothe->item}}
+                                </td>
+                                <td>
+                                    {{$clothe->price}}
+                                </td>
+                                <td>
+                                    {{$clothe->brand}}
+                                </td>
+                                <td>
+                                    {{$clothe->collection_year}}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
